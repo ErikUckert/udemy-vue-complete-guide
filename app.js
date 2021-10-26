3b6f7a1124281d281358a1aec239d14c7e3df0f4
Erik Uckert <erik@Eriks-Air.fritz.box>
2021-10-26 12:57:20 +0200

    add eventlistener by myself

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const Assignment = {
-    data() {
-        name = "Erik",
-        age = 31,
-        futureAge = age + 5,
-        favNum = Math.random().toFixed(2),
-        imgUrl = "https://v3.vuejs.org/logo.png",
-        imgSize = {
-            width: "100",
-            height: "100"
-        }
-        return {name, age, futureAge, favNum, imgUrl, imgSize};
-    }
-}
+/* const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+});
 
-Vue.createApp(Assignment).mount("#assignment")
+app.mount('#events'); */
+
+const eventHandler =  {
+    data() {
+        counter = 0
+        return {counter}
+    },
+    methods: {
+        incCounter() {
+            this.counter++;
+        },
+        decCounter() {
+            this.counter--;
+        }
+    }
+};
+
+app = Vue.createApp(eventHandler);
+app.mount("#events");
